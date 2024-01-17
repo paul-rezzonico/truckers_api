34b2c1df7e24d21b8bf781f4ae6a198b198cecb3
Paul Windows <paul.rezzonico@windows.com>
2024-01-17 15:01:19 +0100

    Config(Application propreties) add configuration for spring using application.propreties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+	kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "com.unilim.info"
@@ -10,7 +11,19 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest:3.2.1")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.1")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.2.1")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0-Beta2")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0-Beta2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
+	testImplementation("junit:junit:4.13.2")
+	testImplementation("org.mockito:mockito-core:5.9.0")
+	testImplementation("io.strikt:strikt-core:0.34.0")
 }
 
 tasks.test {
