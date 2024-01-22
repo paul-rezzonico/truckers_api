e7d69368f36c34476ac2abdc196124439160eb36
Paul REZZONICO <paul.rezzonico@gmail.com>
2024-01-23 00:36:43 +0100

    Feat(Security) add security class to secure data root access

---
+++ src/main/kotlin/security/UserDetailsManager.kt
@@ -0,0 +1,50 @@
+package com.paulrezzonico.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+
+class UserDetailsManager {
+    @Bean
+    fun userDetailsService(bCryptPasswordEncoder: BCryptPasswordEncoder): UserDetailsService {
+        val manager = InMemoryUserDetailsManager()
+        manager.createUser(
+            User.withUsername("user")
+                .password(bCryptPasswordEncoder.encode("userPass"))
+                .roles("USER")
+                .build()
+        )
+        manager.createUser(
+            User.withUsername("admin")
+                .password(bCryptPasswordEncoder.encode("adminPass"))
+                .roles("USER", "ADMIN")
+                .build()
+        )
+        return manager
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers(HttpMethod.GET, "/check_msg").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/check_err").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/sync_msg").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/sync_err").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .httpBasic(withDefaults())
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+        return http.build()
+    }
+
+}
