eea0957e6b2d21062fd8597a67865b308965888c
Paul Windows <paul.rezzonico@windows.com>
2024-01-22 18:40:55 +0100

    Feat(Messages entity) id are now send with request to simply verification after sending data to api

--- src/main/kotlin/model/Message.kt
+++ src/main/kotlin/model/Message.kt
@@ -1,9 +1,7 @@
 package com.paulrezzonico.model
 
-import java.util.*
-
 data class Message(
-    val id: String = UUID.randomUUID().toString(),
+    val id: String,
     val envoyeur: NumeroDeTelephone,
     val message: String,
     val dateReception: String
