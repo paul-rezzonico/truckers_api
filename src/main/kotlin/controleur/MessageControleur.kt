c297cd76ce520f81ad1fa385d927b52d6286b909
Paul Windows <paul.rezzonico@windows.com>
2024-01-22 18:34:26 +0100

    Feat(Errors routes) add routes to errors messages

--- src/main/kotlin/controleur/MessageControleur.kt
+++ src/main/kotlin/controleur/MessageControleur.kt
@@ -14,9 +14,18 @@ class MessageControleur(private val servicesMessage: ServiceMessage) {
     @GetMapping("/check_msg")
     fun recupererToutLesMessages(): List<Destinataire> = servicesMessage.recupererToutLesMessages()
 
+    @GetMapping("/check_err")
+    fun recupererToutLesMessagesEnErreur(): List<Destinataire> = servicesMessage.recupererToutLesMessagesEnErreur()
+
     @PostMapping("/sync_msg")
     fun creerNumeroDeTelephone(@RequestBody payload: NumeroEtMessages): String {
         servicesMessage.mettreAJourMessage(payload.numero, payload.messages)
         return "Messages sauvegardés sur le serveur"
+    }
+
+    @PostMapping("/sync_err")
+    fun creerNumeroDeTelephoneEnErreur(@RequestBody payload: NumeroEtMessages): String {
+        servicesMessage.mettreAJourMessageEnErreur(payload.numero, payload.messages)
+        return "Messages en erreur sauvegardés sur le serveur"
     }
 }
--- src/main/kotlin/service/ServiceMessage.kt
+++ src/main/kotlin/service/ServiceMessage.kt
@@ -10,9 +10,13 @@ import org.springframework.stereotype.Service
 class ServiceMessage(private val numeroDeTelephoneRepository: IMessageRepository) {
     fun recupererToutLesMessages(): List<Destinataire> = numeroDeTelephoneRepository.recupererTout()
 
+    fun recupererToutLesMessagesEnErreur(): List<Destinataire> = numeroDeTelephoneRepository.recupererTouteLesErreurs()
+
     fun recupererMessageParId(id: Long) {
 
     }
 
     fun mettreAJourMessage(destinataire: NumeroDeTelephone, messages: List<Message>): Boolean = numeroDeTelephoneRepository.mettreAJour(destinataire, messages)
+
+    fun mettreAJourMessageEnErreur(destinataire: NumeroDeTelephone, messages: List<Message>): Boolean = numeroDeTelephoneRepository.mettreAJourErreurs(destinataire, messages)
 }
