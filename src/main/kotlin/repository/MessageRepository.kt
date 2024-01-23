fc1688741db194397712714ca3cda32287e84512
Paul REZZONICO <paul.rezzonico@gmail.com>
2024-01-23 22:53:12 +0100

    Feat(FileScheduling) create file scheduling to create new file everyday

--- src/main/kotlin/repository/MessageRepository.kt
+++ src/main/kotlin/repository/MessageRepository.kt
@@ -5,6 +5,7 @@ import com.uniLim.info.model.Destinataire
 import com.uniLim.info.model.Message
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
 class MessageRepository : IMessageRepository {
@@ -12,25 +13,25 @@ class MessageRepository : IMessageRepository {
     @Autowired
     val JsonDataProvider: JsonDataProvider? = null
     override fun recupererTout(): List<Destinataire> {
-        return JsonDataProvider!!.getData("Messages.json")
+        return JsonDataProvider!!.getData("Messages-${LocalDate.now()}.json")
     }
 
     override fun recupererTouteLesErreurs(): List<Destinataire> {
-        return JsonDataProvider!!.getData("Erreurs.json")
+        return JsonDataProvider!!.getData("Erreurs-${LocalDate.now()}.json")
     }
 
     override fun recupererParId(id: String): List<Message> {
-        return JsonDataProvider!!.getData("Messages.json")
+        return JsonDataProvider!!.getData("Messages-${LocalDate.now()}.json")
             .find { it.idTelephone == id }?.messages
             ?: throw NoSuchElementException("Aucun message trouvé pour l'id $id")
     }
 
     override fun mettreAJour(destinataire: String, messages: List<Message>): Boolean {
-        return JsonDataProvider!!.addData(destinataire, messages, "Messages.json")
+        return JsonDataProvider!!.addData(destinataire, messages, "Messages-${LocalDate.now()}.json")
     }
 
     override fun mettreAJourErreurs(destinataire: String, messages: List<Message>): Boolean {
-        return JsonDataProvider!!.addData(destinataire, messages, "Erreurs.json")
+        return JsonDataProvider!!.addData(destinataire, messages, "Erreurs-${LocalDate.now()}.json")
     }
 
     override fun supprimerParNumero(id: Long) {
