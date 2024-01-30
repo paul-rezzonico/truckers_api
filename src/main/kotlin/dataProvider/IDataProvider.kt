ace7d9fffa6446f7ce50d537af0a771d788f251f
Paul Rezzonico <paul.rezzonico@desktop>
2024-01-30 10:35:46 +0100

    Refact(IDataProvider) change return type to match new architecture

--- src/main/kotlin/dataProvider/IDataProvider.kt
+++ src/main/kotlin/dataProvider/IDataProvider.kt
@@ -4,7 +4,6 @@ import com.uniLim.info.model.Destinataire
 import com.uniLim.info.model.Message
 
 interface IDataProvider {
-    fun getData(fileName: String): List<Destinataire>
-
-    fun addData(destinataire : String, messages: List<Message>, fileName: String): Boolean
+    fun getData(fileName: String): Destinataire?
+    fun addData(destinataire: String, messages: List<Message>, fileName: String): Boolean
 }
