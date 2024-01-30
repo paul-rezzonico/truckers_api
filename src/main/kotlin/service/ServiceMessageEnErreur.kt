69f6f241013005f34b905ed5266f867713c21422
Paul Rezzonico <paul.rezzonico@desktop>
2024-01-30 10:37:23 +0100

    Refact(service) remove unused imports

--- src/main/kotlin/service/ServiceMessageEnErreur.kt
+++ src/main/kotlin/service/ServiceMessageEnErreur.kt
@@ -1,6 +1,5 @@
 package com.uniLim.info.service
 
-import com.uniLim.info.model.Destinataire
 import com.uniLim.info.model.Message
 import com.uniLim.info.repository.IMessageRepository
 import org.springframework.stereotype.Service
