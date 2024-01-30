f52e7727524d1f61ab5f058aed49d84b840cf298
Paul Rezzonico <paul.rezzonico@desktop>
2024-01-30 12:50:17 +0100

    Feat(Mailler) add attachment option to mail

--- src/main/kotlin/service/EmailService.kt
+++ src/main/kotlin/service/EmailService.kt
@@ -1,21 +1,29 @@
 package service
 
+import jakarta.mail.MessagingException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.FileSystemResource
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 
 @Service
-class EmailService {
-    @Autowired
-    private val emailSender: JavaMailSender? = null
+class EmailService(@Autowired private val emailSender: JavaMailSender) {
 
-    fun sendSimpleMessage(to: String?, subject: String?, text: String?) {
-        val message = SimpleMailMessage()
-        message.from = "truckers.api@gmail.com"
-        message.setTo(to)
-        message.subject = subject
-        message.text = text
-        emailSender!!.send(message)
+    @Throws(MessagingException::class)
+    fun sendMail(to: String, subject: String, text: String, cheminPieceJointe: String) {
+        val message = emailSender.createMimeMessage()
+        val helper = MimeMessageHelper(message, true)
+
+        helper.setFrom("truckers.api@gmail.com")
+        helper.setTo(to)
+        helper.setSubject(subject)
+        helper.setText(text)
+
+        val file = FileSystemResource(cheminPieceJointe)
+        helper.addAttachment(file.filename ?: "attachment.zip", file)
+
+        emailSender.send(message)
     }
 }
