df169abe05b27a69c94488e8c27b5b0850ea92cb
Paul Rezzonico <paul.rezzonico@desktop>
2024-01-30 10:54:42 +0100

    Refact(Scheduling) remove cron task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
-@EnableScheduling
 open class Main {
 }
 fun main(args: Array<String>) {
