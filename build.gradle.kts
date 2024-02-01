5ff3daf75092f459e62e03786b3dd36394b17de5
Paul Rezzonico <paul.rezzonico@desktop>
2024-02-01 14:40:11 +0100

    Feat(SwaggerFox) add swagger fox to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-	id("org.springframework.boot") version "3.1.5"
+    id("org.springframework.boot") version "3.1.5"
     id("org.jetbrains.kotlin.jvm") version "2.0.0-Beta1"
 }
 
@@ -12,17 +12,19 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.1")
-	implementation("org.springframework.security:spring-security-core:6.2.1")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0-Beta1")
-	implementation("org.springframework.boot:spring-boot-starter-mail:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.1")
+    implementation("org.springframework.security:spring-security-core:6.2.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0-Beta1")
+    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.2")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
-	testImplementation("junit:junit:4.13.2")
-	testImplementation("org.mockito:mockito-core:5.9.0")
-	testImplementation("io.strikt:strikt-core:0.34.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.9.0")
+    testImplementation("io.strikt:strikt-core:0.34.0")
 }
 
 tasks.test {
