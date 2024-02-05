515d00211d6c9eed213809022837d8bd4ea2afbb
Paul Rezzonico <paul.rezzonico@desktop>
2024-02-05 16:00:54 +0100

    Test(FileServingControllerTest) add tests to route

---
+++ src/test/kotlin/com/uniLim/info/integration/FileServingControllerTests.kt
@@ -0,0 +1,48 @@
+package com.uniLim.info.integration
+
+import com.uniLim.info.controller.FileServingController
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.http.HttpHeaders
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.nio.file.Paths
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(FileServingController::class)
+class FileServingControllerTests {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var resourceLoader: ResourceLoader
+
+    @Test
+    fun `when File Exists then Serve File`() {
+        // TODO: Replace with a temporary directory
+    }
+
+    @Test
+    fun `when File Does Not Exist then Return Not Found`() {
+        // Given
+        val filename = "nonExistentFile.json"
+        val resource = mock(Resource::class.java)
+
+        given(resourceLoader.getResource("file:jsonFilePath$filename")).willReturn(resource)
+        given(resource.exists()).willReturn(false)
+
+        mockMvc.perform(get("/public/$filename"))
+                .andExpect(status().isNotFound())
+    }
+}
