45a7edd56df2745470dc032d7f068753ef738a71
paul-rezzonico <paul.rezzo@gmail.com>
2025-03-25 10:25:23 +0100

    Feat(gradle.kts) update dependencies and java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.5"
     id("org.jetbrains.kotlin.jvm") version "2.0.0-Beta1"
     id("org.sonarqube") version "4.4.1.3373"
     id("org.jetbrains.kotlinx.kover") version "0.7.5"
@@ -9,30 +8,38 @@ plugins {
 group = "com.unilim.info"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.1")
-    implementation("org.springframework.security:spring-security-core:6.2.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0-Beta1")
-    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.2")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.4")
+    implementation("org.springframework.security:spring-security-core:6.4.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.20")
+    implementation("org.springframework.boot:spring-boot-starter-mail:3.4.4")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
-    testImplementation("org.mockito:mockito-core:5.9.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.mockito:mockito-core:5.16.1")
     testImplementation("org.mockito:mockito-inline:5.2.0")
-    testImplementation("io.strikt:strikt-core:0.34.0")
+    testImplementation("io.strikt:strikt-core:0.35.1")
     testImplementation("com.google.jimfs:jimfs:1.3.0")
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
 
 sonar {
